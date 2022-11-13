462e2add170a48ee793b69161b22a904b0df3266
MNaufal470 <naufal.sabartudin@gmail.com>
2022-11-14 03:54:23 +0700

    deploy

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,31 +1,35 @@
 export const fetchUserInfo = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getUserInfo`
-  );
-  const data = await res.json();
-  const response = data.pageInfo;
-  return response;
+  // const res = await fetch(
+  //   `${process.env.NEXT_PUBLIC_BASE_URL}/api/getUserInfo`
+  // );
+  // const data = await res.json();
+  // const response = data.pageInfo;
+  // return response;
+  return <h1>Hello</h1>;
 };
 
 export const fetchSocial = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
-  const data = await res.json();
-  const response = data.socials;
-  return response;
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
+  // const data = await res.json();
+  // const response = data.socials;
+  // return response;
+  return <h1>Hello</h1>;
 };
 
 export const fetchSkills = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
-  const data = await res.json();
-  const response = data.skills;
-  return response;
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
+  // const data = await res.json();
+  // const response = data.skills;
+  // return response;
+  return <h1>Hello</h1>;
 };
 
 export const fetchProjects = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
-  );
-  const data = await res.json();
-  const response = data.projects;
-  return response;
+  // const res = await fetch(
+  //   `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
+  // );
+  // const data = await res.json();
+  // const response = data.projects;
+  // return response;
+  return <h1>Hello</h1>;
 };
